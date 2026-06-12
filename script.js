fbd8756115f6d9b804f15fb3923dc25bfbf6316f
MariaMello1234 <maria.mello.mata@escola.pr.gov.br>
2026-06-12 14:43:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function curtirLivro(nomeLivro) {
+    alert(`Você curtiu o livro: ${nomeLivro} ❤️`);
+}
 
+const form = document.getElementById("resenhaForm");
+const listaResenhas = document.getElementById("listaResenhas");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const comentario = document.getElementById("comentario").value;
+
+    const novaResenha = document.createElement("div");
+    novaResenha.classList.add("resenha");
+
+    novaResenha.innerHTML = `
+        <h4>${nome}</h4>
+        <p>${comentario}</p>
+    `;
+
+    listaResenhas.prepend(novaResenha);
+
+    form.reset();
+});
