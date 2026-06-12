01d9142da391d03dc3a4db0aef120bfb8de540bb
MariaMello1234 <maria.mello.mata@escola.pr.gov.br>
2026-06-12 15:53:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,59 @@ document
     );
 
 });
+const livroForm = document.getElementById("livroForm");
+const listaLivros = document.getElementById("listaLivros");
+
+livroForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const titulo =
+        document.getElementById("tituloLivro").value;
+
+    const autor =
+        document.getElementById("autorLivro").value;
+
+    const categoria =
+        document.getElementById("categoriaLivro").value;
+
+    const descricao =
+        document.getElementById("descricaoLivro").value;
+
+    const novoLivro =
+        document.createElement("div");
+
+    novoLivro.classList.add("livro-comunidade");
+
+    novoLivro.innerHTML = `
+        <h3>${titulo}</h3>
+        <p><strong>Autor:</strong> ${autor}</p>
+        <p><strong>Categoria:</strong> ${categoria}</p>
+        <p>${descricao}</p>
+
+        <button onclick="curtirLivro('${titulo}')">
+            ❤️ Curtir
+        </button>
+
+        <button onclick="comentarLivro('${titulo}')">
+            💬 Comentar
+        </button>
+    `;
+
+    listaLivros.prepend(novoLivro);
+
+    livroForm.reset();
+});
+
+function comentarLivro(titulo){
+
+    const comentario =
+        prompt(`Escreva um comentário para "${titulo}"`);
+
+    if(comentario){
+
+        alert(
+            `Comentário enviado para "${titulo}"!`
+        );
+    }
+}
